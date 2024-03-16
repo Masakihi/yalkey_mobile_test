cd67c6e7665f5e47470496863b25ed41c1133ace
Masakihi <lasalle4615@icloud.com>
2024-03-16 11:58:19 +0900

    投稿削除エラー、ミッション登録後のダイアログ修正

--- lib/post/post_detail_page.dart
+++ lib/post/post_detail_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'post_model.dart';
 import 'post_widget.dart';
+import '../home_page.dart';
 
 class PostDetailPage extends StatefulWidget {
   final int postNumber;
@@ -44,13 +45,12 @@ class _PostDetailPageState extends State<PostDetailPage> {
     var _loginUserId = prefs.getString('login_user_id');
     var _loginUserNumber = prefs.getInt('login_user_number');
     setState(() => {
-      loginUserName = _loginUserName,
-      loginUserIconImage = _loginUserIconImage,
-      loginUserId = _loginUserId,
-      loginUserNumber = _loginUserNumber,
-    });
+          loginUserName = _loginUserName,
+          loginUserIconImage = _loginUserIconImage,
+          loginUserId = _loginUserId,
+          loginUserNumber = _loginUserNumber,
+        });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -58,71 +58,77 @@ class _PostDetailPageState extends State<PostDetailPage> {
       appBar: AppBar(
         title: const Text('投稿詳細'),
         actions: [
-          if (loginUserNumber==_postDetailResponse?.post.postUser.postUserNumber)
-          IconButton(
-            onPressed: () {
-              if (_postDetailResponse != null) {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: const Text('削除の確認'),
-                      content: const Text('本当に削除してもよろしいですか？'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('キャンセル'),
-                        ),
-                        TextButton(
-                          onPressed: () async {
-                            await _postDetailResponse!.post.delete();
-                            setState(() => {});
-                            Navigator.pop(context); // 投稿詳細画面を閉じる
-                            Navigator.pop(context);
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text('投稿を削除しました')),
-                            );
-                          },
-                          child: Text('削除'),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              }
-            },
-            icon: const Icon(Icons.delete),
-          ),
-          if (loginUserNumber==_postDetailResponse?.post.postUser.postUserNumber)
-          IconButton(
-            onPressed: () async {
-              if (_postDetailResponse != null) {
-                await _postDetailResponse!.post.pin();
-                setState(() => {});
-                ScaffoldMessenger.of(context).removeCurrentSnackBar();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                      content: Text(_postDetailResponse!.post.postPinned!
-                          ? '投稿をピン止めしました'
-                          : 'ピン止めを解除しました')),
-                );
-              }
-            },
-            icon: Icon(
-              _postDetailResponse != null
-                  ? _postDetailResponse!.post.postPinned!
-                      ? Icons.push_pin
-                      : Icons.push_pin_outlined
-                  : Icons.push_pin_outlined,
-              color: _postDetailResponse != null
-                  ? _postDetailResponse!.post.postPinned!
-                      ? Colors.red
-                      : Colors.grey
-                  : Colors.grey,
+          if (loginUserNumber ==
+              _postDetailResponse?.post.postUser.postUserNumber)
+            IconButton(
+              onPressed: () {
+                if (_postDetailResponse != null) {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('削除の確認'),
+                        content: const Text('本当に削除してもよろしいですか？'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('キャンセル'),
+                          ),
+                          TextButton(
+                            onPressed: () async {
+                              await _postDetailResponse!.post.delete();
+                              setState(() => {});
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => HomePage(),
+                                ),
+                              );
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text('投稿を削除しました')),
+                              );
+                            },
+                            child: Text('削除'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+              },
+              icon: const Icon(Icons.delete),
             ),
-          ),
+          if (loginUserNumber ==
+              _postDetailResponse?.post.postUser.postUserNumber)
+            IconButton(
+              onPressed: () async {
+                if (_postDetailResponse != null) {
+                  await _postDetailResponse!.post.pin();
+                  setState(() => {});
+                  ScaffoldMessenger.of(context).removeCurrentSnackBar();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                        content: Text(_postDetailResponse!.post.postPinned!
+                            ? '投稿をピン止めしました'
+                            : 'ピン止めを解除しました')),
+                  );
+                }
+              },
+              icon: Icon(
+                _postDetailResponse != null
+                    ? _postDetailResponse!.post.postPinned!
+                        ? Icons.push_pin
+                        : Icons.push_pin_outlined
+                    : Icons.push_pin_outlined,
+                color: _postDetailResponse != null
+                    ? _postDetailResponse!.post.postPinned!
+                        ? Colors.red
+                        : Colors.grey
+                    : Colors.grey,
+              ),
+            ),
         ],
       ),
       body: _isLoading
